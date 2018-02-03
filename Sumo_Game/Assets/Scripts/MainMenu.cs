1c0600425522503544d925915ae519c6c17d9358
Hunter Ahlquist <contact@hunterahlquist.com>
2018-02-02 20:30:41 -0800

    + Folder for holding sprites. + Basic main menu system. + Fonts folder with a Gabriola added
    The menu in my personal scene has a button for changing the scene to "Main Scene" (Play,) and a button for quitting the game via Application.Quit

--- Sumo_Game/Assets/Scripts/MainMenu.cs
+++ Sumo_Game/Assets/Scripts/MainMenu.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("Scenes/MainScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
 }
