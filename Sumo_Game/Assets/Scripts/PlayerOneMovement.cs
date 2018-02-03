7ad1581d890f5967b46850aa54428891d4693693
Cohav0310 <36090325+Cohav0310@users.noreply.github.com>
2018-02-02 18:16:42 -0800

    Added Code
    I programmed the function of pressing the A key to move a rigidbody (Player) to the left and D to move to the right.
    This change has only been made to the Player one Script

--- Sumo_Game/Assets/Scripts/PlayerOneMovement.cs
+++ Sumo_Game/Assets/Scripts/PlayerOneMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerOneMovement : MonoBehaviour {
 
+
+    public float moveSpeed;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -12,5 +15,15 @@ public class PlayerOneMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
-	}
+        if(Input.GetKeyDown (KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, 0);
+        }
+
+    }
 }
