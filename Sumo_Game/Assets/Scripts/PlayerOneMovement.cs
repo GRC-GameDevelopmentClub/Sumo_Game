b8099a49fdb30d3799bb80b17f3d2ed224fb708a
Cohav0310 <36090325+Cohav0310@users.noreply.github.com>
2018-02-09 16:41:54 -0800

    Jump
    I added a function in the script to jump. It has a system that checks to see if you are on the ground. I put the ground on a new layer called "ground". I updated the prefabs with these changes.

--- Sumo_Game/Assets/Scripts/PlayerOneMovement.cs
+++ Sumo_Game/Assets/Scripts/PlayerOneMovement.cs
@@ -6,30 +6,47 @@ public class PlayerOneMovement : MonoBehaviour {
 
 
     public float moveSpeed;
+    public float jumpHeight;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool grounded;
+
+    private Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
         if(Input.GetKey (KeyCode.A))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, 0);
+            rb.velocity = new Vector2(-moveSpeed, 0);
         }
         
         else if (Input.GetKey(KeyCode.D))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, 0);
+            rb.velocity = new Vector2(moveSpeed, 0);
         }
         else
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+            rb.velocity = new Vector2(0, rb.velocity.y);
         }
 
-        
+        if (Input.GetKeyDown(KeyCode.W) && grounded)
+        {
+            rb.velocity = new Vector2(0, jumpHeight);
+        }
+
 
 
     }
--- Sumo_Game/Assets/Scripts/PlayerTwoMovement.cs
+++ Sumo_Game/Assets/Scripts/PlayerTwoMovement.cs
@@ -5,26 +5,46 @@ using UnityEngine;
 public class PlayerTwoMovement : MonoBehaviour {
 
     public float moveSpeed;
+    public float jumpHeight;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool grounded;
+
+    private Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody2D>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+
+    // Update is called once per frame
+    void Update () {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, 0);
+            rb.velocity = new Vector2(-moveSpeed, 0);
         }
 
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, 0);
+            rb.velocity = new Vector2(moveSpeed, 0);
         }
         else
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+            rb.velocity = new Vector2(0, rb.velocity.y);
         }
+
+        if(Input.GetKeyDown(KeyCode.UpArrow) && grounded)
+        {
+            rb.velocity = new Vector2(0, jumpHeight);
+        }
+
     }
 }
